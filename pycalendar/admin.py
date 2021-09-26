04030a5672240b91d0f9d1724afbda34ff21785b
Madhav Khandhar <madhavkhandhar@gmail.com>
2021-09-26 18:58:32 +0800

    Created calendar models and admin pages Added pycache to gitignore

--- pycalendar/admin.py
+++ pycalendar/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from pycalendar.models import * 
 
-# Register your models here.
+class TitleInline(admin.StackedInline):
+    model = Title
+
+class DescriptionMarkdownInline(admin.StackedInline):
+    model = DescriptionMarkdown
+
+class EventAdmin(admin.ModelAdmin):
+    inlines = [
+        TitleInline,
+        DescriptionMarkdownInline
+    ]
+
+class CategoryTitleInline(admin.StackedInline):
+    model = CategoryTitle
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [CategoryTitleInline]
+    
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Language)
--- pycalendar/models.py
+++ pycalendar/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class Language(models.Model):
+    short_code = models.CharField(max_length=10)
+    language = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.short_code} {self.language}"
+
+#English name should never be used, it is just for backend ease of use
+#CategoryTitle of the appropriate language should be used instead
+class Category(models.Model):
+    english_name = models.CharField(max_length=255)
+    hex_color = models.CharField(max_length=6)
+
+    def __str__(self):
+        return self.english_name
+
+class CategoryTitle(models.Model):
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Event(models.Model):
+    article = models.URLField(blank=True, null=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+#Multiple titles per event to support multiple languages
+class Title(models.Model):
+    language = models.ForeignKey(Language, on_delete=models.SET_NULL, null=True)
+    title = models.TextField()
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+#Multiple descriptions per event to support multiple languages
+class DescriptionMarkdown(models.Model):
+    language = models.ForeignKey(Language, on_delete=models.SET_NULL, null=True)
+    description_markdown = models.TextField()
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
